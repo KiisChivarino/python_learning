e99948e96d74833928f89ae85c2a64af4bee9be7
lmax <kiischivarino@gmail.com>
2022-02-12 18:32:09 +0500

    task3

--- lesson_2/task3.py
+++ lesson_2/task3.py
@@ -4,7 +4,7 @@ seasons = {'зима': [12, 1, 2], 'весна': [3, 4, 5], 'лето': [6, 7, 8
 
 while True:
     user_value = input('Введите номер месяца\n')
-    if not (user_value.strip().isdigit() and (1 > int(user_value) or int(user_value) > 12)):
+    if not (user_value.strip().isdigit()) and (1 > int(user_value) or int(user_value) > 12):
         print(f'Значение [{user_value}] должно быть целым положительным числом от 1 до 12!\n')
         continue
     user_month = int(user_value)
