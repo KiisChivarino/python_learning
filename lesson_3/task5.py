9427bab72cc815109b7398e6a4f61a14b9146717
lmax <kiischivarino@gmail.com>
2022-02-12 18:29:21 +0500

    task5

--- lesson_3/task5.py
+++ lesson_3/task5.py
@@ -1,11 +1,5 @@
 def words_title(string_of_words):
-    return ' '.join(
-        map(
-            str.title, list(
-                filter(None, string_of_words.split(' '))
-            )
-        )
-    )
+    return ' '.join(map(str.title, list(filter(None, string_of_words.split(' ')))))
 
 
 print(words_title('слово1 слово2 слово3'))
