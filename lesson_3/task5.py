2a3b357924d14706fe8022320dcbae51063d915c
kiischivarino <kiischivarino@gmail.com>
2022-06-08 16:13:34 +0300

    refactoring

--- lesson_3/task5.py
+++ lesson_3/task5.py
@@ -4,11 +4,12 @@ import re
 def sum_a_string_of_numbers(string_of_numbers, separator=' '):
     return sum(list(map(int, list(filter(None, string_of_numbers.split(separator))))))
 
+
 sum_of_numbers = int()
 print('Добавляйте числа через пробел; для завершения добавьте точку')
 while True:
     string_of_numbers = input()
-    if re.search('[^0-9 .]', string_of_numbers):
+    if re.search('[^\d .]', string_of_numbers):
         print('Необходимо вводить только числа через пробел или точку!')
         continue
     if '.' in string_of_numbers:
--- lesson_3/task6.py
+++ lesson_3/task6.py
@@ -1,5 +1,6 @@
 def words_title(string_of_words):
-    return ' '.join(map(str.title, list(filter(None, string_of_words.split(' ')))))
+    return str.title(' '.join(list(filter(None, string_of_words.split(' ')))))
 
 
-print(words_title('слово1 слово2 слово3'))
+print(words_title('слово1 '
+                  'слово2   слово3'))
